2417b54c74270f3fa343559dcd834facd606ef1c
Danil <you@gmail.com>
2024-08-11 20:13:10 +0300

    add people num

--- src/components/CardSection/CardSection.jsx
+++ src/components/CardSection/CardSection.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import "./CardSectiom.css";
-import Transport from "../Transport/Transport";
+import TravelSection from "../TravelSection/TravelSection";
 
 
 
 function CardSection(props) {
     console.log(props);
+    {props.people && (<div> {props.people} </div>)}
   return (
+    
     <div className="allCard">
         <img src ={props.img} alt="" className="img"/>
-        <div className="transports" >{props.transportName}</div>
+        <div className="position-card">
+          <div className="flex-card">
+          <div className="transports" >{props.transportName} </div>
+          <div className="transports__people">{props.people} {props.peopleNum}</div>
+
+          </div>
+        </div>
+    
         <p className="title">{props.title} {props.price}</p>
         
     </div>
--- src/components/TravelSection/TravelSection.jsx
+++ src/components/TravelSection/TravelSection.jsx
@@ -3,6 +3,7 @@ import photo1 from "../../assets/1.png";
 import photo2 from "../../assets/2.png";
 import photo3 from "../../assets/3.png";
 import photo4 from "../../assets/4.png";
+import icon from "../../assets/icon.png";
 import CardSection from '../CardSection/CardSection';
 import "./TravelSection.css";
 
@@ -11,25 +12,35 @@ const cities = [
         img: photo1,
         transportName: 'на автобусе',
         title: "Тбилиси, апрель —",
-        price: 83000
+        price: 83000,
+        people: true,
+        peopleNum:10,
+        icon: icon
     },
     {
         img: photo2,
         transportName: 'на самолете',
         title: "Стамбул, март —",
-        price: 110000
+        price: 110000,
+        people: false
     },
     {
         img: photo3,
         transportName: 'на самолете',
         title: "Дубай, июнь —",
-        price: 220000
+        price: 220000,
+        people: true,
+        peopleNum:15,
+        icon: icon
     },
     {
         img: photo4,
         transportName: 'самолет + паром',
         title: "Пхукет, сентябрь — ",
-        price: 135000
+        price: 135000,
+        people: true,
+        peopleNum:11,
+        icon: icon
     }
 ];
 
@@ -44,6 +55,9 @@ function TravelSection() {
                 transportName = {city.transportName}
                 title={city.title} 
                 price={city.price}
+                people = {city.people}
+                peopleNum = {city.peopleNum}
+                icon = {city.icon}
                  />
             ))}
         </div>
